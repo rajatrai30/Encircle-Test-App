9550e831288d74b1e267367a6813eb71ad88069d
rajatrai30 <mailmerairajat@mail.com>
2023-10-11 12:50:32 +0530

    init

--- client/src/socket.io/connection.js
+++ client/src/socket.io/connection.js
@@ -4,7 +4,7 @@ let SERVER_URL = "https://encircle-test-app.vercel.app/";
 
 console.log("Node environment:", process.env.NODE_ENV)
 if (process.env.NODE_ENV !== "production"){
-    SERVER_URL = "https://encircle-test-app.vercel.app/";
+    SERVER_URL = "http://localhost:3001/";
 }
 
 export const socket = io.connect(SERVER_URL);
